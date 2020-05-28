8d6d6e6a75d9f3c5efe58ddfbfe3eae74eb4b80b
Christoph Hellwig <hch@lst.de>
2020-05-28 07:12:13 +0200

    BACKPORT: net: add sock_bindtoindex
    Add a helper to directly set the SO_BINDTOIFINDEX sockopt from kernel
    space without going through a fake uaccess.
    
    Change-Id: Idb5a895062810cb89b3b7dca8a774b8b2c6161d5
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sock.h
+++ include/net/sock.h
@@ -2618,7 +2618,6 @@ static inline void sk_pacing_shift_update(struct sock *sk, int val)
 int sockev_register_notify(struct notifier_block *nb);
 int sockev_unregister_notify(struct notifier_block *nb);
 
-/* On 32bit arches, an skb frag is limited to 2^15 */
-#define SKB_FRAG_PAGE_ORDER	get_order(32768)
+int sock_bindtoindex(struct sock *sk, int ifindex);
 
 #endif	/* _SOCK_H */
