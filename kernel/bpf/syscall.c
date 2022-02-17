d4df55e44e63bb8fc469e4fde4c28331e097c35c
Eric Dumazet <edumazet@google.com>
2022-02-17 10:19:02 -0800

    UPSTREAM: bpf: Add schedule points in batch ops
    commit 75134f16e7dd0007aa474b281935c5f42e79f2c8 upstream.
    
    syzbot reported various soft lockups caused by bpf batch operations.
    
     INFO: task kworker/1:1:27 blocked for more than 140 seconds.
     INFO: task hung in rcu_barrier
    
    Nothing prevents batch ops to process huge amount of data,
    we need to add schedule points in them.
    
    Note that maybe_wait_bpf_programs(map) calls from
    generic_map_delete_batch() can be factorized by moving
    the call after the loop.
    
    This will be done later in -next tree once we get this fix merged,
    unless there is strong opinion doing this optimization sooner.
    
    Fixes: aa2e93b8e58e ("bpf: Add generic support for update and delete batch ops")
    Fixes: cb4d03ab499d ("bpf: Add generic support for lookup batch op")
    Reported-by: syzbot <syzkaller@googlegroups.com>
    Change-Id: I42bc3d3289b850d09c082d9282b82758efd229bc
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Reviewed-by: Stanislav Fomichev <sdf@google.com>
    Acked-by: Brian Vazquez <brianvv@google.com>
    Link: https://lore.kernel.org/bpf/20220217181902.808742-1-eric.dumazet@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1361,6 +1361,7 @@ int generic_map_delete_batch(struct bpf_map *map,
 		maybe_wait_bpf_programs(map);
 		if (err)
 			break;
+		cond_resched();
 	}
 	if (copy_to_user(&uattr->batch.count, &cp, sizeof(cp)))
 		err = -EFAULT;
@@ -1418,6 +1419,7 @@ int generic_map_update_batch(struct bpf_map *map,
 
 		if (err)
 			break;
+		cond_resched();
 	}
 
 	if (copy_to_user(&uattr->batch.count, &cp, sizeof(cp)))
@@ -1515,6 +1517,7 @@ int generic_map_lookup_batch(struct bpf_map *map,
 		swap(prev_key, key);
 		retry = MAP_LOOKUP_RETRIES;
 		cp++;
+		cond_resched();
 	}
 
 	if (err == -EFAULT)
